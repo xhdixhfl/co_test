f2a3d5774de54c878fd1b09a4d899fb2cb95bd7d
océan(갱갱) <114147352+xhdixhfl@users.noreply.github.com>
2023-01-12 09:55:41 +0900

    Update 피자 나눠 먹기 （1）.py

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -4,3 +4,10 @@ def solution(n):
     else:
         answer = n//7 + 1
     return answer
+
+
+def solution(n):
+    # 리스트내포의 if-else문 [참인경우 참의 조건 거짓 조건]
+    answer = [n//7 + 0 if n%7 == 0 else n//7 + 1][0]
+
+    return answer
