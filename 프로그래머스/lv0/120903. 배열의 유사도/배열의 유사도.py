614bb30eb7c2e3cb1bcc3ef31a15ddbb6ab003c2
océan(갱갱) <114147352+xhdixhfl@users.noreply.github.com>
2023-01-13 10:11:16 +0900

    Update 배열의 유사도.py

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,5 +1,8 @@
 def solution(s1, s2):
     x = set(s1) 
     y = set(s2)
-    
+# 이런시도도 했었음.
+    # a1= [x for i in s2 for x in s1 if x in i]
+    # a2= [x for i in s1 for x in s2 if x in i]
+    # answer = [x for i in a2 for x in a1 if x in i]    
     return len(x & y)
