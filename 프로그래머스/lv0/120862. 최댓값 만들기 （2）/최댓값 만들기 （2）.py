b581f7b9a3ce90391c5af6e4183f604fc8be0ce6
océan(갱갱) <114147352+xhdixhfl@users.noreply.github.com>
2023-01-18 10:17:16 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    numbers.sort()
+    a = numbers[-1]*numbers[-2]
+    b = numbers[0]*numbers[1]
+    if a>b:
+        answer = a
+    else:
+        answer = b
+    return answer
