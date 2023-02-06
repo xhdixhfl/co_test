ee6cd263a7d1f3230bc25e3e2f1b8292c1aaad50
océan(갱갱) <114147352+xhdixhfl@users.noreply.github.com>
2023-02-06 09:58:39 +0900

    Update 공 던지기.py

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -1,3 +1,4 @@
 def solution(numbers, k):
     # players = numbers[0::2] if len(numbers) % 2 ==0 else numbers[0::2] + numbers[1::2]
+    # return players[(k % len(players)) -1]
     return numbers[2 * (k - 1) % len(numbers)]
