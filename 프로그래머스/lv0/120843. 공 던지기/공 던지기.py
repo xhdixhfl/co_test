844b96ab3e8d30151ec2c06912b391a4ca43679c
océan(갱갱) <114147352+xhdixhfl@users.noreply.github.com>
2023-02-06 09:57:11 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -1,18 +1,3 @@
 def solution(numbers, k):
-    # answer = 0
-    # if len(numbers) % 2 == 0:
-    #     if len(numbers) // 2 < k:
-    #         answer = (k - (len(numbers)/2)) + 1
-    #     else:
-    #         answer = ((len(numbers)/2) - k) + 1
-    # else:
-    #     if k > len(numbers):
-    #         a = k % len(numbers)
-    #     k % 2 == 1:
-    #         answer = 2 * (k//2)
-    #     else
-    #     while k > len(numbers):
-    #         k -= len(numbers)
-    #         answer = ((len(numbers)//2) - k) + 1
-              
-    return ((k-1)*2) % len(numbers)
+    # players = numbers[0::2] if len(numbers) % 2 ==0 else numbers[0::2] + numbers[1::2]
+    return numbers[2 * (k - 1) % len(numbers)]
