aaa83f4b87c228ce13b1223f449a2be6533fa2eb
océan(오갱이) <114147352+xhdixhfl@users.noreply.github.com>
2023-02-20 22:55:07 +0900

    Update 캐릭터의 좌표.py

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -18,3 +18,22 @@ def solution(keyinput, board):
 
     return [x, y]
     return one, two
+
+# 이렇게 도전중 근데 8번이 계속 탈락됨
+    # one = []
+    # two = []
+    # answer = [one.append(1) if i == 'right' else one.append(-1) if i == 'left' else two.append(1) if i == 'up' else two.append(-1) for i in keyinput]
+    # if board[0]//2 >= abs(sum(one)):
+    #     one = sum(one)
+    # else:
+    #     if sum(one)>0:
+    #         one = board[0]//2
+    #     else:
+    #         one = -(board[0]//2)
+    # if board[1]//2 >= abs(sum(two)):
+    #     two = sum(two)
+    # else:
+    #     if sum(two)>0:
+    #         two = board[1]//2
+    #     else:
+    #         two = -(board[1]//2)
