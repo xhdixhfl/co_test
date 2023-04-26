e7ffb49301260ab7d6529d0820d82638849f0588
오갱이(océan) <114147352+xhdixhfl@users.noreply.github.com>
2023-04-26 19:08:16 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT *
+FROM PLACES
+WHERE HOST_ID IN (SELECT HOST_ID FROM PLACES 
+                  GROUP BY HOST_ID 
+                  HAVING COUNT(DISTINCT(ID)) >=2)
+ORDER BY ID
