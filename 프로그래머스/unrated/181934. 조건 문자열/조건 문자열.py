4865758e0cbf3a79c7cdb04066a52da1d6f7d46c
오갱이(océan) <114147352+xhdixhfl@users.noreply.github.com>
2023-06-08 20:32:48 +0900

    [unrated] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,26 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    if ineq =='>' and eq == '=':
+        if n >= m:
+            return 1
+        else:
+            return 0
+    elif ineq =='<' and eq == '=':
+        if n <= m:
+            return 1
+        else:
+            return 0
+        
+    elif ineq =='>' and eq == '!':
+        if n > m:
+            return 1
+        else:
+            return 0
+    elif ineq =='<' and eq == '!':
+        if n < m:
+            return 1
+        else:
+            return 0
+    else:
+        return 0        
+        
